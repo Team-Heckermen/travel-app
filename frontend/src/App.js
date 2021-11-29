51c8fb97565079e719c97f87af53bff782914bbd
Dheer <dheerm309@gmail.com>
2021-11-29 12:46:06 +0530

    react router setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
-      <p>welcome</p>
+      <Router>
+        <Route component={Home} exact path="/" />
+        <Route component={Login} exact path="/login" />
+      </Router>
     </div>
   );
 }
