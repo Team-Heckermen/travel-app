70143c01d670406e9e5dc7bf535ae8bba9859403
Dheer <dheerm309@gmail.com>
2021-12-03 21:10:09 +0530

    refresh and rotate the token

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -7,17 +7,19 @@ const AuthContext = createContext();
 export default AuthContext;
 
 export const AuthProvider = ({ children }) => {
+  let [loading, setLoading] = useState(true);
   let [user, setUser] = useState(() =>
     localStorage.getItem("authTokens")
       ? jwt_decode(localStorage.getItem("authTokens"))
       : null
   );
-  let [themeIsDark, setThemeIsDark] = useState(false);
   let [authTokens, setAuthTokens] = useState(() =>
     localStorage.getItem("authTokens")
       ? JSON.parse(localStorage.getItem("authTokens"))
       : null
   );
+
+  let [themeIsDark, setThemeIsDark] = useState(false);
 
   const history = useHistory();
 
@@ -55,12 +57,48 @@ export const AuthProvider = ({ children }) => {
     history.push("/login");
   };
 
+  let updateToken = async () => {
+    console.log("update token called");
+    let response = await fetch("http://127.0.0.1:8000/api/token/refresh/", {
+      method: "POST",
+
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify({
+        refresh: authTokens.refresh,
+      }),
+    });
+
+    let data = await response.json();
+
+    if (response.status === 200) {
+      setAuthTokens(data);
+      setUser(jwt_decode(data.access));
+    } else {
+      logoutUser();
+    }
+  };
+
   let contextData = {
     user: user,
     themeIsDark: themeIsDark,
     loginUser: loginUser,
     logoutUser: logoutUser,
   };
+
+  useEffect(() => {
+    let time = 1000 * 60 * 4; // 4 mins
+    let interval = setInterval(() => {
+      if (authTokens) {
+        updateToken();
+      }
+    }, time);
+
+    return () => clearInterval(interval);
+  }, [authTokens, loading]);
+
   return (
     <AuthContext.Provider value={contextData}>{children}</AuthContext.Provider>
   );
