93508dea26bea6ba23323729ddf6d0e8f27db988
pritansh sahsani <82302289+pritansh-sahsani@users.noreply.github.com>
2022-02-07 19:15:27 +0530

    styled logo and navbar

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import AuthContext from "../context/AuthContext";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   let { loginUser } = useContext(AuthContext);
@@ -16,7 +17,6 @@ const Login = () => {
               placeholder="example@email.com"
               className="sign-in-input"
               name="email"
-              value={email}
               required
             />
             <label className="block mt-3 font-semibold">Password</label>
@@ -25,7 +25,6 @@ const Login = () => {
               placeholder="password123"
               className="sign-in-input"
               name="password"
-              value={password}
               minLength="7"
               required
             />
