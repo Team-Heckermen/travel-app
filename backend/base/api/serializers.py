d07959bb063c3f75a3791d9c6f2187908ca195a3
Dheer <dheerm309@gmail.com>
2022-05-01 20:04:56 +0530

    email compatibility, and login with both email and username

--- backend/base/api/serializers.py
+++ backend/base/api/serializers.py
@@ -9,6 +9,10 @@ class RegisterSerializer(serializers.ModelSerializer):
         required=True,
         validators=[UniqueValidator(queryset=User.objects.all())]
     )
+    username = serializers.CharField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
 
     password = serializers.CharField(
         write_only=True, required=True, validators=[validate_password])
@@ -16,7 +20,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'password2', 'email')
+        fields = ('username','password', 'password2', 'email')
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
@@ -27,8 +31,8 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create(
-            username=validated_data['username'],
             email=validated_data['email'],
+            username=validated_data['username']
         )
 
         user.set_password(validated_data['password'])
