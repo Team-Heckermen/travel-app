c93d44d21a0c10741e59321475cc8b556e6ab118
Dheer <dheerm309@gmail.com>
2022-05-30 20:02:42 +0530

    .env file load bug fixed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,6 +14,10 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
+# Load .env files and environment variables
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -68,8 +72,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # email id info
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('PASSWORD')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
